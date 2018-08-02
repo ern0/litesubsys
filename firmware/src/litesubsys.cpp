30f08cadacad653675ff569613285fe417499757
ern0 <ern0@linkbroker.hu>
2018-08-02 14:46:08 +0200

    button event gen missing

--- firmware/src/litesubsys.cpp
+++ firmware/src/litesubsys.cpp
@@ -2,6 +2,10 @@
 #include <Adafruit_NeoPixel.h>
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(1,LIGHT_PIN,NEO_GRB | NEO_KHZ800);
+static char powerState = OFF;
+static char lightState = OFF;
+static char singleElemEventBuffer;
+
 
 void setup() {
 
@@ -13,22 +17,101 @@ void setup() {
   strip.begin();
   strip.setBrightness(30);
 
+	powerOff();
+	lightOff();
 
 } // setup()
 
 
 void loop() {
 
-  digitalWrite(ONBOARD_PIN,HIGH);
-  digitalWrite(POWER_PIN,HIGH);
-  strip.setPixelColor(0, 255,255,0);
-  strip.show();
-  delay(200);
+	clearEvent();
 
-  digitalWrite(ONBOARD_PIN,LOW);
-  digitalWrite(POWER_PIN,LOW);
-  strip.setPixelColor(0, 0,0,0);
-  strip.show();
-  delay(700);
+	pollSerial();
+	pollButtons();
+
+	switch (singleElemEventBuffer) {
+	case E_POWER_ON:
+		powerOn();
+		break;
+	case E_POWER_OFF:
+		powerOff();
+		break;
+	case E_LIGHT_ON:
+		lightOn();
+		break;
+	case E_LIGHT_OFF:
+		lightOff();
+		break;
+	}
+
+  delay(100);
 
 } // loop()
+
+
+inline void clearEvent() {
+	singleElemEventBuffer = E_NONE;
+} // clearEvent()
+
+
+inline void fire(char event) {
+	singleElemEventBuffer = event;
+} // fire()
+
+
+
+inline void pollSerial() {
+
+  char c = Serial.read();
+
+  if (c == 'p') fire(E_POWER_OFF);
+  if (c == 'P') fire(E_POWER_ON);
+  if (c == 'l') fire(E_LIGHT_OFF);
+  if (c == 'L') fire(E_LIGHT_ON);
+
+} // pollSerial()
+
+
+inline void pollButtons() {
+
+	/// TODO
+
+} // pollButtons()
+
+
+inline void powerOn() {
+
+	digitalWrite(POWER_PIN,HIGH);
+	powerState = ON;
+
+} // powerOn()
+
+
+void powerOff() {
+
+	digitalWrite(POWER_PIN,LOW);
+	lightOff();
+	powerState = OFF;
+
+} // powerOff()
+
+
+inline void lightOn() {
+
+	if (powerState == OFF) return;
+
+	strip.setPixelColor(0, 255,40,0);
+	strip.show();
+	lightState = ON;
+
+} // lightOn()
+
+
+inline void lightOff() {
+
+	strip.setPixelColor(0, 0,0,0);
+	strip.show();
+	lightState = OFF;
+
+} // lightOff()
--- firmware/src/litesubsys.hpp
+++ firmware/src/litesubsys.hpp
@@ -8,4 +8,27 @@
 # define POWER_BUTTON 6
 # define LIGHT_BUTTON 7
 
+enum {
+	E_NONE,
+	E_POWER_OFF,
+	E_POWER_ON,
+	E_LIGHT_OFF,
+	E_LIGHT_ON
+};
+
+enum {
+	ON,
+	OFF
+};
+
+inline void clearEvent();
+inline void fire(char event);
+inline void pollSerial();
+inline void pollButtons();
+
+inline void powerOn();
+inline void powerOff();
+inline void lightOn();
+inline void lightOff();
+
 # endif
